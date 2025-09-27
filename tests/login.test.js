b669dd967a56623031da08c04586c1cae85252c0
weberthwsm <weberthwsm@gmail.com>
2025-09-27 20:26:41 -0300

    adicionando stages ao teste

--- tests/login.test.js
+++ tests/login.test.js
@@ -3,12 +3,21 @@ import { sleep,check } from 'k6';
 
 
 export const options = {
-
-
 //  iterations: 50,  / executa 50 chamadas sequenciais
-  vus: 10,    // simula 10 usuários simultaneos
-  duration: '30s',
-  thresholds: {
+//vus: 10,    // simula 10 usuários simultaneos
+//  duration: '30s',
+
+    stages:[
+        {duration: '10s', target: 10},
+        {duration: '20s', target: 10},
+        {duration: '10s', target: 30},
+        {duration: '20s', target: 1000},
+        {duration: '20s', target: 0}
+
+    ],
+
+
+    thresholds: {
     
     http_req_duration: ['p(95)<3000', 'max <5000'], // 95% of requests should be below 200ms
     http_req_failed: ['rate<0.01'], // http errors should be less than 1%
