fd17d634fb8f3f4acbdd7ee31858650bad3c2b8c
weberthwsm <weberthwsm@gmail.com>
2025-09-27 17:16:01 -0300

    Adiciondo o arquivo inicial com teste de login

---
+++ tests/login.test.js
@@ -0,0 +1,39 @@
+import http from 'k6/http';
+import { sleep,check } from 'k6';
+
+
+export const options = {
+  iterations: 50,
+ thresholds: {
+    
+    http_req_duration: ['p(95)<7', 'max <8'], // 95% of requests should be below 200ms
+    http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+  },
+};
+
+
+export default function () {
+  const url = 'http://localhost:3000/login';
+  const payload = JSON.stringify({
+    username: 'julio.lima',
+    senha: '123456',
+  });
+
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  
+ const res = http.post(url, payload, params);
+  sleep(1);
+
+console.log(res);
+
+  check(res, {
+    'Validar que status é 200': (r) => r.status === 200,
+    'Validar que o Token é string':(r) => typeof(r.json().token) == 'string'
+  });
+
+    
+}
